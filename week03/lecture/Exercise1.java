7bd403047c5a73f633f780dd4c3733df0fbc7488
B-zen00 <banphot7483@gmail.com>
2025-11-08 10:06:37 +0700

    week3

--- week03/lecture/Exercise1.java
+++ week03/lecture/Exercise1.java
@@ -1,7 +1,14 @@
 package week03.lecture;
+import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input your budget = ");
+        int budget = input.nextInt();
+        if(budget >= 15000) {
+            System.out.printf("Your budget = %d ,you can buy it",budget);
+        }
+        input.close();
     }
 }
